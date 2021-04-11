e71c699eb6c6aa24d790661ae0e230425d429747
saleh elnaggar <saleh.elnaggar@gmail.com>
2021-04-11 18:18:11 +0200

    first commit

---
+++ 1_storageBackend/variables.tf
@@ -0,0 +1,11 @@
+variable "project_code" {
+  default = "mugh"
+}
+
+variable "environment_code" {
+  default = "test"
+}
+
+variable "location" {
+  default = "east us"
+}
---
+++ 2_network/variables.tf
@@ -0,0 +1,15 @@
+variable "project_code" {
+  default = "mugh"
+}
+
+variable "environment_code" {
+  default = "rg"
+}
+
+variable "network_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "agents_subnet_cidr" {
+  default = "10.0.0.0/24"
+}
